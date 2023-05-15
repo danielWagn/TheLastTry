553e8054e74ca958d486515ae62a937fbca2f1da
danielWagn <danielwagner9979@gmail.com>
2023-05-15 19:24:34 +0200

    frontend client

--- Frontend/client.js
+++ Frontend/client.js
@@ -1,5 +1,20 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.listen(3000, () => console.log('Anwendung hoert auf Port 3000'));
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname) + "/Programm.html");
+});
+
+app.get('/detail', (req,res,) => {
+    res.sendFile(path.join(__dirname) + "/Programm.html");
+});
+
+app.get('/', (req,res,) => {
+    res.sendFile(path.join(__dirname) + "/Programm.html");
+});
+
+app.listen(3000, () => console.log('Web listens on Port 3000'));
