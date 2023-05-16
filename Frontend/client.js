77151c78fba0a3116ec1f0c7607ca507f565f077
danielWagn <danielwagner9979@gmail.com>
2023-05-16 20:14:28 +0200

    dynamische programmseite

--- Frontend/client.js
+++ Frontend/client.js
@@ -5,16 +5,13 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req,res) => {
+app.get('/filme', (req,res) => {
+    res.sendFile(path.join(__dirname) + "/programm_new.html");
+});
+
+app.get('/filme/details', (req,res,) => {
     res.sendFile(path.join(__dirname) + "/Programm.html");
 });
 
-app.get('/detail', (req,res,) => {
-    res.sendFile(path.join(__dirname) + "/Programm.html");
-});
-
-app.get('/', (req,res,) => {
-    res.sendFile(path.join(__dirname) + "/Programm.html");
-});
 
 app.listen(3000, () => console.log('Web listens on Port 3000'));
---
+++ Frontend/filmProgramm.js
@@ -0,0 +1,6 @@
+let programm = document.createElement('div');
+programm.id = 'programm-box';
+programm.className = 'row movie-box';
+programm.innerHTML = 'Hello';
+
+let root = document.querySelector('#');
